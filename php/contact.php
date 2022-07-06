2395ead782fd477d2bd3a65405e2c6d15ced5a08
lasitha851 <lasithadilshan851@gmail.com>
2022-07-06 10:08:56 +0400

    update projects page

--- php/contact.php
+++ php/contact.php
@@ -42,7 +42,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "example@example.net";
+$address = "lasithadilshan851@gmail.com";
 
 
 // Configuration option.
